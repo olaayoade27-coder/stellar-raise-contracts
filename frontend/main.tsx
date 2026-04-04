93cb72d299c25e5a523f5efd911decbe3d9d52b4
afurious <misraisiguzor@gmail.com>
2026-04-04 13:52:57 +0100

    resolve error

---
+++ frontend/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { FrontendGlobalErrorBoundary } from './components/frontend_global_error';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <FrontendGlobalErrorBoundary>
+      <App />
+    </FrontendGlobalErrorBoundary>
+  </React.StrictMode>,
+);
